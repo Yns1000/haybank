e250d5e769eaa9a1ef3020dea126dadf66c90d17
Yns1000 <younes.bgrt@icloud.com>
2025-06-16 15:02:30 +0200

    [UPDATE] - Improve authentication by normalizing table name and adding debug logging for token

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -5,7 +5,9 @@ module.exports = (req, res, next) => {
     const token = req.headers['authorization'];
     if (!token) return res.status(401).json({ error: 'Token manquant' });
 
-    db.query('SELECT * FROM Utilisateur WHERE hashcode = ?', [token], (err, results) => {
+    db.query('SELECT * FROM utilisateur WHERE hashcode = ?', [token], (err, results) => {
+        // print le token pour debug
+        console.log('Token reçu:', token);
         if (err || results.length === 0) return res.status(401).json({ error: 'Token invalide' });
         req.user = results[0];
         next();
